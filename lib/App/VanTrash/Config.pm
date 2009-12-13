8e68826816fd7490b781c84ccb4e1ecfa3cc055d
Kevin Jones <kvn.jns@gmail.com>
2009-12-13 15:46:37 -0800

    instead of an environment variable, check the path to see if we're in a dev-env

--- lib/App/VanTrash/Config.pm
+++ lib/App/VanTrash/Config.pm
@@ -1,9 +1,11 @@
 package App::VanTrash::Config;
 use base 'Exporter';
 use YAML;
+use FindBin;
 
 my $CONFIG_FILE => '/etc/vantrash.yaml';
 my $CONFIG = -e $CONFIG_FILE ? YAML::LoadFile($CONFIG_FILE) : {};
+my $IS_DEV = $FindBin::Bin && $FindBin::Bin =~ m{^/home};
 
 my $base_url;
 sub base_url {
@@ -11,7 +13,7 @@ sub base_url {
     return $base_url if $base_url;
 
     $base_url = $CONFIG->{base_url} || 'http://vantrash.ca';
-    if ($ENV{DEV_ENV}) {
+    if ($IS_DEV) {
         my $port = 1009 + $<;
         $base_url .= ":$port";
     }
