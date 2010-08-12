7fdecba0f8c0b3dc0b0cced07eb5826fbffa11e7
Kevin Jones <kvn.jns@gmail.com>
2010-08-11 17:54:59 -0700

    Shrink the text to give more room to the ad

--- lib/App/VanTrash/Controller.pm
+++ lib/App/VanTrash/Controller.pm
@@ -23,7 +23,7 @@ has 'request' => (is => 'rw', isa => 'HTTP::Engine::Request');
 has 'logger' =>
     (default => sub { App::VanTrash::Log->new }, handles => ['log']);
 
-our $VERSION = 1.4;
+our $VERSION = 1.5;
 
 sub handle_request {
     my $self = shift;
