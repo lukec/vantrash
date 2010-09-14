8dbaa525cd094ba90ad5e83141b3fd399ef877fc
Luke Closs <gravatar@5thplane.com>
2010-09-14 11:34:55 -0700

    Update to use twitter oauth

--- lib/App/VanTrash/Twitter.pm
+++ lib/App/VanTrash/Twitter.pm
@@ -11,12 +11,26 @@ has 'twitter' => (is => 'ro', lazy_build => 1,
 sub _build_twitter {
     my $self = shift;
 
-    return Net::Twitter->new(
-        traits => ['WrapError', 'API::REST'],
+    my $nt = Net::Twitter->new(
+        traits    => [ 'WrapError', 'API::REST', 'OAuth' ],
         useragent => 'VanTrash',
-        map { $_ => App::VanTrash::Config->Value("twitter_$_") }
-            qw/username password/
+        consumer_key => 
+            App::VanTrash::Config->Value("twitter_consumer_key"),
+        consumer_secret =>
+            App::VanTrash::Config->Value("twitter_consumer_secret"),
     );
+
+    my $access_token = App::VanTrash::Config->Value('twitter_oauth_token');
+    my $token_secret
+        = App::VanTrash::Config->Value('twitter_oauth_token_secret');
+    if ($access_token && $token_secret) {
+        $nt->access_token($access_token);
+        $nt->access_token_secret($token_secret);
+    }
+    unless ($nt->authorized) {
+        die "Twitter OAuth client is not authorized. Update Vantrash config.\n";
+    }
+    return $nt;
 }
 
 __PACKAGE__->meta->make_immutable;
