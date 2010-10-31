272dce36c37206999381c77d74de8f8c688b69f6
Luke Closs <github@5thplane.com>
2010-10-31 08:04:49 -0700

    update bootstrap to install apt & perl deps

--- bin/vantrash-bootstrap.sh
+++ bin/vantrash-bootstrap.sh
@@ -2,9 +2,44 @@
 set -x
 set -e
 
+# 
 # Script to bootstrap vantrash installation on a EC2 instance
+#
 
-sudo apt-get install --assume-yes git-core screen nginx tmpreaper libxml2-dev libdb4.6-dev
+# First install core system dependencies
+sudo apt-get install --assume-yes git-core screen nginx tmpreaper libxml2-dev libdb4.6-dev ctags
+
+# Now install Perl dependencies
+curl -L http://cpanmin.us | perl - --sudo App::cpanminus
+cpanm --sudo  \
+BerkeleyDB \
+Crypt::DES \
+DBIx::Class \
+Data::ICal \
+Data::UUID \
+Date::ICal \
+Email::MIME \
+Email::MIME::Creator \
+Email::Send \
+Email::Send::Gmail \
+Email::Valid \
+JavaScript::Minifier::XS \
+MRO::Compat \
+Math::Polygon \
+MooseX::Types \
+MooseX::Types::Common::String \
+Net::Twitter \
+Socialtext::WikiObject \
+Socialtext::WikiTest \
+Template \
+Test::HTTP \
+URI::Encode \
+WWW::Shorten \
+WWW::Shorten::isgd \
+WWW::Twilio::API \
+Web::Scraper \
+XML::XPath \
+mocked
 
 cd $HOME
 if [[ ! -d src ]]; then
@@ -14,11 +49,13 @@ cd src
 
 if [[ ! -d dotdotdot ]]; then
 	git clone git@github.com:lukec/dotdotdot.git
-	rm dotdotdot/README
+	rm -f dotdotdot/README
 	mv dotdotdot/{.[A-z]*,*} $HOME
+        rm -rf dotdotdot
 fi
 
 if [[ ! -d vantrash ]]; then
 	git clone git@github.com:lukec/vantrash.git
 fi
+
 
