e3fea41ede78dcd403dae10c8cfb048685865cf2
Luke Closs <github@5thplane.com>
2010-10-31 10:52:48 -0700

    more updates from ec2 usage

--- bin/vantrash-bootstrap.sh
+++ bin/vantrash-bootstrap.sh
@@ -7,39 +7,51 @@ set -e
 #
 
 # First install core system dependencies
-sudo apt-get install --assume-yes git-core screen nginx tmpreaper libxml2-dev libdb4.6-dev ctags
+sudo apt-get install --assume-yes git-core nginx tmpreaper libxml2-dev \
+        libdb4.6-dev ctags libssl-dev sqlite3 daemontools-run
+# And some helper dev packages
+sudo apt-get install --assume-yes screen multitail
 
 # Now install Perl dependencies
+# First install cpanm for easy cpanning
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
-cpanm --sudo  \
-BerkeleyDB \
-Crypt::DES \
-DBIx::Class \
-Data::ICal \
-Data::UUID \
-Date::ICal \
-Email::MIME \
-Email::MIME::Creator \
-Email::Send \
-Email::Send::Gmail \
-Email::Valid \
-JavaScript::Minifier::XS \
-MRO::Compat \
-Math::Polygon \
-MooseX::Types \
-MooseX::Types::Common::String \
-Net::Twitter \
-Socialtext::WikiObject \
-Socialtext::WikiTest \
-Template \
-Test::HTTP \
-URI::Encode \
-WWW::Shorten \
-WWW::Shorten::isgd \
-WWW::Twilio::API \
-Web::Scraper \
-XML::XPath \
-mocked
+
+# Then install some missing dep
+cpanm --sudo --force \
+    Module::Install::ReadmeMarkdownFromPod
+
+# Now get to the meat of our Perl dependiencies
+cpanm --sudo \
+    BerkeleyDB \
+    Crypt::DES \
+    DBIx::Class \
+    Data::ICal \
+    Data::UUID \
+    Date::ICal \
+    Email::MIME \
+    Email::MIME::Creator \
+    Email::Send \
+    Email::Send::Gmail \
+    Email::Valid \
+    JavaScript::Minifier::XS \
+    MRO::Compat \
+    Math::Polygon \
+    MooseX::Types \
+    MooseX::Singleton \
+    MooseX::Types::Common::String \
+    Net::Twitter \
+    Socialtext::WikiObject \
+    Socialtext::WikiTest \
+    Template \
+    Test::HTTP \
+    URI::Encode \
+    WWW::Shorten \
+    WWW::Shorten::isgd \
+    WWW::Twilio::API \
+    Web::Scraper \
+    XML::XPath \
+    mocked \
+    Plack
 
 cd $HOME
 if [[ ! -d src ]]; then
@@ -58,4 +70,6 @@ if [[ ! -d vantrash ]]; then
 	git clone git@github.com:lukec/vantrash.git
 fi
 
+git config --global alias.co 'checkout'
+git config --global color.ui true
 
